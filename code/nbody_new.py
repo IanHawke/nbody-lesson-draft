7743edda2ff1b1e036517ca8747ae797becdc41f
Ian Hawke <I.Hawke@soton.ac.uk>
2015-05-16 13:54:14 +0100

    Sketch of a better (?!) code.

---
+++ code/nbody_new.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 12:19:34 2015
+
+@author: Ian Hawke, Ashwin Srinath
+"""
+
+import numpy
+
+def dqdt(r, v, m):
+    """
+    Compute the derivatives of velocity and position.
+    
+    Parameters
+    ----------
+    
+    r : list of float
+        Positions (3 N)
+    v : list of float
+        Velocities (3 N)
+    m : list of float
+        Masses (N)
+        
+    Returns
+    -------
+    
+    drdt : list of float
+        Derivative of positions (3 N)
+    dvdt : list of float
+        Derivative of velocities (3 N)
+    """
+    
+    N = len(m)
+    drdt = numpy.zeros_like(r)
+    dvdt = numpy.zeros_like(v)
+    dr = numpy.zeros((3,))
+    for i in range(N):
+        for k in range(3):
+            drdt[i][k] = v[i][k]
+        for j in range(i):
+            for k in range(3):
+                dr[k] = r[i][k] - r[j][k]
+            mag = (dr[0]**2 + dr[1]**2 + dr[2]**2)**(-0.5)
+            for k in range(3):
+                dvdt[i][k] += mag * m[j] * dr[k]
+                dvdt[j][k] += mag * m[i] * dr[k]
+    
+    return drdt, dvdt
+
+def advance(dt, n, bodies):
+    """
+    Step forward as in the original N-body code.
+    """
+    
+    r = numpy.array(bodies[0])
+    v = numpy.array(bodies[1])
+    m = numpy.array(bodies[2])
+    for step in range(n):
+        drdt, dvdt = dqdt(r, v, m)
+        r += dt * drdt
+        v += dt * dvdt
+    
+    return [list(r), list(v), list(m)]
+    
